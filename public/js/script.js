36c7f061bfcc4dda3cdeda581120c2745519c1d0
Shaun Pienaar <sfpienaar1990@gmail.com>
2020-09-08 17:19:06 +0200

    Updated the look of site

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,63 @@ function dropDown(){
     x.className = 'nav';
   }
 }
+
+function switchClass(){
+  //let tops = document.getElementsByClassName('top');
+  let proj = document.getElementById('project1');
+  let tops = proj.childNodes;
+  for(let i = 0; i < tops.length; i++){
+    if(tops[i].className == 'top'){
+      tops[i].className='bottom';
+      continue;
+    }
+    if(tops[i].className == 'bottom'){
+      tops[i].className='top';
+    }
+  }
+}
+
+function switchClass2(){
+  //let tops = document.getElementsByClassName('top');
+  let proj = document.getElementById('project2');
+  let tops = proj.childNodes;
+  for(let i = 0; i < tops.length; i++){
+    if(tops[i].className == 'top'){
+      tops[i].className='bottom';
+      continue;
+    }
+    if(tops[i].className == 'bottom'){
+      tops[i].className='top';
+    }
+  }
+}
+
+function switchClass3(){
+  //let tops = document.getElementsByClassName('top');
+  let proj = document.getElementById('project3');
+  let tops = proj.childNodes;
+  for(let i = 0; i < tops.length; i++){
+    if(tops[i].className == 'top'){
+      tops[i].className='bottom';
+      continue;
+    }
+    if(tops[i].className == 'bottom'){
+      tops[i].className='top';
+    }
+  }
+}
+
+function switchClass4(){
+  //let tops = document.getElementsByClassName('top');
+  let proj = document.getElementById('project4');
+  let tops = proj.childNodes;
+  for(let i = 0; i < tops.length; i++){
+    if(tops[i].className == 'top'){
+      tops[i].className='bottom';
+      continue;
+    }
+    if(tops[i].className == 'bottom'){
+      tops[i].className='top';
+    }
+  }
+}
